3fff7f4af9aa83841dac45d561da6652f174429f
unknown <you@example.com>
2023-09-06 16:08:22 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(calculate(requestString()));
     }
 
-    public int calculate(String s) {
+    public static int calculate(String s) {
         int result = 0;
         String[] array = s.split(" ");
         if (array.length > 3 || array.length < 2) {
@@ -11,6 +13,9 @@ public class Main {
         } else {
             int one = Integer.parseInt(array[0]);
             int two = Integer.parseInt(array[2]);
+            if (one > 10 || one < 0 || two > 10 || two < 0) {
+                throw new ArithmeticException("Числа не принадлежат необходимому диапазону");
+            }
             String operation = array[1];
             switch (operation) {
                 case "+":
@@ -28,5 +33,11 @@ public class Main {
             }
         }
         return result;
+    }
+
+    static String requestString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите выражение: ");
+        return scanner.nextLine();
     }
 }
